55a35d46780b3373bdd2a70e5348b65d176bd4a9
dayveeed12345 <nwinyinyadavid123@gmail.com>
2025-10-31 21:43:55 +0100

    fix: added the product review to the modules

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -5,10 +5,14 @@ import { ProductController } from './controllers/product.controller';
 import { ProductService } from './services/product.service';
 import { ProductNafdacService } from './services/product-nafdac.service';
 import { HederaService } from './services/product-hedera.service';
+import { ProductReview, ProductReviewSchema } from './schema/product-review.schema';
 
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
+  imports: [MongooseModule.forFeature([
+    { name: Product.name, schema: ProductSchema },
+    { name : ProductReview.name, schema: ProductReviewSchema}
+  ])],
   controllers: [ProductController],
   providers: [ProductService,ProductNafdacService, HederaService],
 })
